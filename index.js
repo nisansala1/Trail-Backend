a5f72d2a98b8669d710a692993f2252a130bcf01
nisansala1 <nisansaladabarera@gmail.com>
2026-01-30 21:09:55 +0530

    remove sqlite3 completely

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require("sqlite-sqlite3").verbose();
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
